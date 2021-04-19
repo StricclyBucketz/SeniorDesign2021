d6a1bf6fefb6c968e26087f45b111167f1cffaa5
robot <christian00white@gmail.com>
2021-04-19 14:42:12 -0500

    Changes to be committed: 	modified:   functions.py

--- src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/functions.py
+++ src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/functions.py
@@ -31,7 +31,7 @@ def speed_control(ID, speed):
         Command('', ID, 'Moving_Speed', speed)
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
-   
+
 def position_control(ID, position):
     rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
     try:
@@ -52,18 +52,18 @@ def publisher():
     pub.publish(message)
     rate.sleep()
     rospy.spin()
-    
+
 def motor_printer(motorSub):
-    rate = rospy.Rate(10)  
+    rate = rospy.Rate(10)
     rospy.loginfo("Motor 7 Position: %s" % (motorSub.dynamixel_state[0].present_position))
-        
+
 def motor_subscriber():
     rospy.init_node('motorSub', anonymous=True)
     posSub = rospy.Subscriber("dynamixel_workbench/dynamixel_state", msg.DynamixelStateList, motor_printer)
     rospy.spin()
 
 def imu_printer(sub):
-    rate = rospy.Rate(10)  
+    rate = rospy.Rate(10)
     rospy.loginfo("Pitch: %s" % (sub.vector.x))
     rospy.loginfo("Roll: %s" % (sub.vector.y))
     rospy.loginfo("Yaw: %s" % (sub.vector.z))
@@ -75,19 +75,25 @@ def IMU_subscriber():
 
 if __name__ == "__main__":
     #to see examples of the functions running, just uncomment them
-    
+
     #Gets data from IMU, you can use access it by using sub.vector.x etc.
     #IMU_subscriber()
-    
+
     #Gets current position from motors, access with posSub.dynamixel_state[index].parameter, these are all listed in AX.msg
     #pos_subscriber()
-    
+
     #Moves motor, arguments are motor ID and goal position (0, 1023)
     #position_control(1, 512)
-    
+
     #Sets motor speed, arguments are motor ID and goal speed (0, 1023)
     #speed_control(1, 512)
-    
+
+    speed_control(1, 512)
+    position_control(1, 450)
+    time.sleep(2)
+    speed_control(1, 20)
+    position_control(1, 512)
+
     #Me attempting to combine the above functions, it appears to work.
     #(ID, Speed, Position)
     #speed_pos_control(1, 512, 512)
