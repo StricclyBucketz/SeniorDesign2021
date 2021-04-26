b013f9962d6639908723f2d41b3c84545310cdad
robot <christian00white@gmail.com>
2021-04-26 00:05:11 -0500

    Changes to be committed: 	modified:   walkLogic.py

--- src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/walkLogic.py
+++ src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/walkLogic.py
@@ -23,7 +23,7 @@ from functions import speed_control, position_control
 DELAY = 0.225
 TOLERANCE = 10 # %tolerance = TOLERANCE / 1023 (In this case ~0.3%)
 BASE_MOTOR_SPEED = 128 # Sets the speed for the servo moving the farthest in a state change.
-STATIC = np.array([512,312,712])    # Default for not {2,3,4,8,9,10}, constant for 3, constant for 9
+STATIC = np.array([512,337,687])    # Default for not {2,3,4,8,9,10}, constant for 3, constant for 9
 # TWO_POS = np.array([622,572])       # Default for servo 2, its extended state
 TWO_POS = np.array([647,597])       # Default for servo 2, its extended state
 # FOUR_POS = np.array([412,362])      # Default for servo 4, its extended state
