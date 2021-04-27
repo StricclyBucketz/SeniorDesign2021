9c5da324e4735524399d0163af2716cfefcb8294
robot <christian00white@gmail.com>
2021-04-27 03:38:26 -0500

    added substates to new.py

--- src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/new.py
+++ src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/new.py
@@ -158,9 +158,9 @@ def walk_ready():
 def F_S_L():
 
   position_control(1, 360)
-  position_control(5, 560)
-  position_control(7, 570)
-  position_control(11, 570)
+  position_control(5, 565)
+  position_control(7, 575)
+  position_control(11, 575)
   time.sleep(0.055)
   # #Substate 32.1
   # position_control(1, 513)
