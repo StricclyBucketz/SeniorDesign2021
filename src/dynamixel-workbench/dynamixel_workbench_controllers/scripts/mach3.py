328291c94599416f0bc1b97a9e145ad989f8c501
robot <christian00white@gmail.com>
2021-04-27 04:07:20 -0500

    new file again

--- src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/mach3.py
+++ src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/mach3.py
@@ -209,7 +209,7 @@ def F_S_L_MID():
   position_control(5, 508)
 
   #position_control(7, 515)
-  position_control(8, 369)
+  position_control(8, 379)
   position_control(9, 873)
   position_control(10, 767)
   position_control(11, 555)
