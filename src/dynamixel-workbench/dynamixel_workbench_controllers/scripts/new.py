68b82a4c1b898a64cf1f164b31e5ab843b97b226
robot <christian00white@gmail.com>
2021-04-26 23:46:24 -0500

    changed functions.py

--- src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/new.py
+++ src/dynamixel-workbench/dynamixel_workbench_controllers/scripts/new.py
@@ -30,7 +30,7 @@ class PosSpeed(IntEnum):
 
 DELAY = 0.225
 TOLERANCE = 10 # %tolerance = TOLERANCE / 1023 (In this case ~0.3%)
-BASE_MOTOR_SPEED = 64 # Sets the speed for the servo moving the farthest in a state change.
+BASE_MOTOR_SPEED = 128 # Sets the speed for the servo moving the farthest in a state change.
 STATIC = np.array([512])    # Default for not {2,3,4,8,9,10}
 # TWO_POS = np.array([622,572])       # Default for servo 2, its extended state
 TWO_POS = np.array([647,577])       # Default for servo 2, its extended state
